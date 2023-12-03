a252f13d7335750406c60fb061333a8679fe62cf
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-12-03 17:32:00 +0100

    ci: Added annotations on lint check

--- scripts/clang-format-check.sh
+++ scripts/clang-format-check.sh
@@ -8,12 +8,17 @@ FILE_EXT="cpp|cc|h|hpp|cxx|c++|c"
 # Run clang-format
 clang-format -dry-run --style=file:./.clang-format -Werror $(git ls-files | grep -E "\.($FILE_EXT)$") 2> error.log
 
-# print badly formatted file names
-# line example: src/utils/Matrix.cpp:285:17: error: code should be clang-formatted [-Wclang-format-violations]
-# expected: src/utils/Matrix.cpp
-
+# print badly formatted file names, if any
 if [ -s error.log ]; then
-    echo "The following files are not clang-formatted:"
-    cat error.log | grep -E "\.($FILE_EXT):[0-9]+:[0-9]+: error:" | awk -F ":" '{print $1}' | sort | uniq
+    cat error.log | grep -E "\.($FILE_EXT):[0-9]+:[0-9]+: error:" | awk -F ":" '{print $1}' | sort | uniq > error_files.log
+    # annotate the files with error, for github actions
+    (
+        while read file; do
+            echo "::error file=$file::clang-format check failed"
+        done
+    ) < error_files.log
+    rm error_files.log
+    rm error.log
     exit 1
 fi
+rm error.log
