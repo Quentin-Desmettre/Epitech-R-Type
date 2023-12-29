abd01eb33e4f58777986b81180021997b8c1a612
Noa Trachez <noa.trachez@epitech.eu>
2023-12-29 18:33:55 +0100

    fix(help): Edit help message to fit with server rules

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,10 +4,9 @@
 int main(int ac, char **av)
 {
     ArgParser parser(ac, av);
-    std::cout << "ip: " << parser.getIp() << std::endl;
     std::cout << "port: " << parser.getPort() << std::endl;
     if (parser.getHelp()) {
-        std::cout << "USAGE: ./rtype_client [-h | --help] [--ip ip] [--port port]" << std::endl;
+        std::cout << "USAGE: ./rtype_client [-h | --help] [--port port]" << std::endl;
         return 0;
     }
     rtype::RTypeServer server(60);
