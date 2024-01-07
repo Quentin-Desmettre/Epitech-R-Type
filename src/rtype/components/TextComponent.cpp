276bf0b9c87db0e1805cf5196f4f438f7e1f4317
Clément <clement.vandeville@epitech.eu>
2024-01-07 18:40:17 +0100

    feat: Text component to display text

---
+++ src/rtype/components/TextComponent.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Clément Vandeville on 07/01/2024.
+//
+
+#include "rtype/components/TextComponent.hpp"
+
+#include <utility>
+
+namespace rtype {
+
+    sf::Font TextComponent::font;
+    bool TextComponent::loadFont = font.loadFromFile("assets/fonts/TT Lakes Neue Trial Black.ttf");
+
+    TextComponent::TextComponent(std::string string, int fontSize, sf::Color color, int zIndex) :
+            zIndex(zIndex),
+            _string(std::move(string)),
+            _fontSize(fontSize),
+            _color(color)
+    {
+        text.setString(_string);
+        text.setCharacterSize(_fontSize);
+        text.setFillColor(_color);
+
+        if (!loadFont) return;
+        text.setFont(font);
+    }
+} // namespace rtype
